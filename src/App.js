a3eb36b9f2fa0dc907c424cc6c576c759eb7dba4
meranamerohith <sairohith10@outllok.com>
2024-03-29 19:02:41 +0530

    routing app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,13 @@
 import logo from './logo.svg';
-import './App.css';
-
+import React from 'react'; 
+import './App.css'; 
+ 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div style={{textAlign:"center"}} >
+        <h3>Welcome to SPA using React JS</h3>          
+        <img src="/Images/Banner.jpg" width="90%" height="250" alt="Alternate text" />               
+   </div>  
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -3,11 +3,50 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import About from './components/About';
+import Contact from './components/Contact';
+import NotFound from './components/NotFound';
+import Emps from './components/Emps';
+import Depts from './components/Depts';
+import Details from './components/Details';
+
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+
+const routing = (
+  <Router>
+
+    <div style={  {textAlign : "center"}  }>
+      <h3>Routing Implementation in React JS</h3>
+      <hr/>
+      <Link to="/">Home</Link>  |
+      <Link to="/Emps">Employees</Link>  |
+      <Link to="/Depts">Departments</Link>  |
+      <Link to="/About">About Us</Link>  |
+      <Link to="/Contact">Contact Us</Link>  |
+      <Link to="/Hello">Invalid</Link>  |
+      <hr />
+    </div>
+
+
+    <Routes>
+      <Route path="/" element={<App />} />
+      <Route path="/Emps" element={<Emps />} />
+      <Route path="/Depts" element={<Depts />} />
+      <Route path="/About" element={<About />} />
+      <Route path="/Contact" element={<Contact />} />
+      <Route path="/Details/:id" element={<Details />} />
+      <Route path="*" element={<NotFound />} />
+    </Routes>
+    
+  </Router>
+
+);
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    {routing}
   </React.StrictMode>
 );
 
