58830c6d5e78b500d0d735ca7f6806ceae2cc3a7
sairohith ambati <164610025+sairohith10@users.noreply.github.com>
2024-06-14 03:41:03 +0000

    images correction

--- src/components/NotFound.js
+++ src/components/NotFound.js
@@ -1,7 +1,7 @@
 function  NotFound(){
     return <div align="center">
       <h3>Requested Page not found</h3>
-      <img src="/Images/NotFound.png" width="90%" height="250" alt="Alternate text" />               
+      <img src="/images/NotFound.PNG" width="90%" height="250" alt="Alternate text" />               
     </div>;
   };
 
